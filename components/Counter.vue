490cbccf39d47e899a9b1ea69b17a560a9b8bc16
azumber <ahmet.zumberoglu@gmail.com>
2021-11-18 13:34:45 +0300

    counter component added

---
+++ components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container w-72 h-72 shadow-2xl rounded place-self-center">
+        <h1 class="text-6xl m-8 justify-center place-self-center">{{ count }}</h1>
+        <div class="flex space-x-4 justify-center">
+            <button @click="increase" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">INCREASE</button>
+            <button @click="decrease" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" >DECREASE</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Counter",
+    data(){
+        return{
+            count: 0
+        }
+    },
+    methods:{
+        increase(){ this.count = this.count + 1 },
+        decrease(){ this.count = this.count - 1 }  
+    }
+}
+</script>
+<style scoped>
+.container {
+    height: 150px;
+    margin-top: 50px;
+    margin-left: 100px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <Tutorial/>
+  <div>
+    <Counter/>
+  </div>
 </template>
 
 <script>
-export default {}
+
+export default {
+}
 </script>
