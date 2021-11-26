9858dbd8c037e996151eda7d664aec1b3017f0ab
azumber <ahmet.zumberoglu@gmail.com>
2021-11-26 14:28:16 +0300

    acceptence test removed

--- Acceptence.js
+++
@@ -1,9 +0,0 @@
-const { openBrowser, goto, click, closeBrowser } = require('taiko');
-
-(async () => {
-    await openBrowser();
-    await goto("http://localhost:3000/");
-    await click("INCREASE");
-    await click("DECREASE");
-    await closeBrowser();
-})();
