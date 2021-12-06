6736543949bbe61207d2f69492c959953aaddd9c
azumber <ahmet.zumberoglu@gmail.com>
2021-12-06 11:35:01 +0300

    header content added

--- src/Request.js
+++ src/Request.js
@@ -2,12 +2,21 @@ import axios from "axios"
 
 export async function getCounter(){
     return axios.get("/display",{
-        baseURL: process.env.apiBaseURL
+        baseURL: process.env.apiBaseURL,
+        headers: { Accept: "application/json" }
     })
 }
 
 export async function counterIncrease(){
     return axios.get("/increase",{
-        baseURL: process.env.apiBaseURL
+        baseURL: process.env.apiBaseURL,
+        headers: { Accept: "application/json" }
+    })
+}
+
+export async function counterDecrease(){
+    return axios.get("/decrease",{
+        baseURL: process.env.apiBaseURL,
+        headers: { Accept: "application/json" }
     })
 }
